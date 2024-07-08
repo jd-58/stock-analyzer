1eddb9d9a4de712f91e16dab9f6a835d00a4ba5d
jd-58 <jacobdeaton58@gmail.com>
2024-07-08 17:00:08 -0400

    Created gui_test.py to test GUI elements with tkinter.

--- gui_test.py
+++ gui_test.py
@@ -9,7 +9,7 @@ from tkinter import *
 root = Tk()
 
 # Creating a label widget
-myLabel = Label(root, text="Hello")
+myLabel = Label(root, text="Hello world")
 
 # Putting it on the screen
 myLabel.pack()
