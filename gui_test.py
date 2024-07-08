3e30991616d92e5c2aa851495b17a4c98a0d2010
jd-58 <jacobdeaton58@gmail.com>
2024-07-08 17:16:45 -0400

    Updated GUI test to assign a function to a button.

--- gui_test.py
+++ gui_test.py
@@ -8,12 +8,23 @@ from tkinter import *
 
 root = Tk()
 
+
+# Function for the button to do something
+def click():
+    number = 5
+    print(number + 5)
+
+
 # Creating a label widget
-myLabel1 = Label(root, text="Hello world")
-myLabel2 = Label(root, text="Test")
+# myLabel1 = Label(root, text="Hello world")
+# myLabel2 = Label(root, text="Test")
+
+# Creating a button
+myButton = Button(root, text="Click Me!", command=click)
 
 # Putting it on the screen
-myLabel1.grid(row=0, column=0)
-myLabel2.grid(row=1, column=0)
+# myLabel1.grid(row=0, column=0)
+# myLabel2.grid(row=1, column=0)
+myButton.pack()
 
 root.mainloop()
